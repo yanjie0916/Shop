00fc28312020f05cb4b1e0f119ef25132628957a
yanjie0916 <s105213062@mail1.ncnu.edu.tw>
2018-10-24 11:27:31 +0800

    期中考練習

---
+++ userModel.php
@@ -0,0 +1,31 @@
+<?php
+require("dbconfig.php");
+function login($id, $pwd) 
+{
+    global $db;
+    $_SESSION['uID']='';
+    $_SESSION['role']='';
+    if ($id>"") {
+        $sql = "select * from user where loginID=? and password=?";
+        $stmt = mysqli_prepare($db, $sql);
+        mysqli_stmt_bind_param($stmt, "ss", $id, $pwd);
+        mysqli_stmt_execute($stmt); //執行SQL
+        $result = mysqli_stmt_get_result($stmt); 
+        $r=mysqli_fetch_assoc($result);
+        if($r){
+            $_SESSION['uID'] = $r['uid'];
+            $_SESSION['role'] = $r['role'];
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    return 0;
+}
+
+function getRole($id) 
+{
+    return $_SESSION['role'];
+}
+
+?>
